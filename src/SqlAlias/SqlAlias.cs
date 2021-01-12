64acece2f352095671f1745d1b4e33acad18b271
Michael Fastrup <mfa@danskecommodities.com>
2021-01-12 14:00:04 +0100

    Added support for .NET 5

--- src/SqlAlias/SqlAlias.cs
+++ src/SqlAlias/SqlAlias.cs
@@ -2,6 +2,7 @@ using System;
 #if !NET40
 using Microsoft.Data.SqlClient;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 #endif
 
 namespace SqlAlias
@@ -13,9 +14,13 @@ namespace SqlAlias
 #if NET40
             return connectionString;
 #else
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return connectionString;
 
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) &&
-                RuntimeInformation.FrameworkDescription.StartsWith(".NET Core")) // netstandard library may be used from NetFX runtime
+            Match match = Regex.Match(RuntimeInformation.FrameworkDescription, @"^(?<product>.NET(?: Core)?) (?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+).*$");
+            if (match.Success
+                && (string.Equals(match.Groups["product"].Value, ".NET Core", StringComparison.OrdinalIgnoreCase) // netstandard library may be used from NetFX runtime
+                    || string.Equals(match.Groups["product"].Value, ".NET", StringComparison.OrdinalIgnoreCase) && int.TryParse(match.Groups["major"].Value, out int major) && major >= 5)
+            )
             {
 
                 try
