079661abd5ec240addaf62523761ebbea2f6c61e
Manfred Pohler <manniat@live.com>
2021-06-30 10:21:07 +0200

    Update SqlAlias.cs
    copy code to repo

--- src/SqlAlias/SqlAlias.cs
+++ src/SqlAlias/SqlAlias.cs
@@ -4,7 +4,20 @@ using Microsoft.Data.SqlClient;
 using System.Runtime.InteropServices;
 
 #endif
+#if false
+    if(OperatingSystem.IsWindows() && strConString.Contains("source=syn")) {
+				var builder = new SqlConnectionStringBuilder(strConString);
 
+				var key = Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE") != "x86"
+					? @"HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\MSSQLServer\Client\ConnectTo"
+					: @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\MSSQLServer\Client\ConnectTo";
+
+				if(Microsoft.Win32.Registry.GetValue(key, builder.DataSource, null) is string newSource) {
+					builder.DataSource = newSource.Substring(newSource.IndexOf(',') + 1);
+				}
+				strConString = builder.ConnectionString;
+    
+#endif
 namespace SqlAlias
 {
     public static class Aliases
